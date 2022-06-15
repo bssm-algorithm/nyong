d6766318bed6ceb1451014b250849ea613b59f3f
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-06-15 23:58:49 +0900

    DAY :: (#03)

---
+++ algorithm/src/backjoon/배열/BackJoon_1546.java
@@ -0,0 +1,34 @@
+package backjoon.배열;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BackJoon_1546 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Float> arr = new ArrayList<>();
+        Float max = Float.valueOf(0);
+        float sum = 0, ave;
+
+        for (int i = 0; i < n; i ++){
+            arr.add(sc.nextFloat());
+        }
+
+        for (int j = 0; j < n; j ++){
+            if (arr.get(j) > max){
+                max = arr.get(j);
+            }
+        }
+
+        for (int k = 0; k < arr.size(); k ++){
+            arr.set(k, arr.get(k)/max*100);
+
+            sum += arr.get(k);
+        }
+
+        ave = sum/arr.size();
+        System.out.println(ave);
+    }
+}
