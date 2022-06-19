dc4452ce9caec0c32192370c350f84e198ead856
Woowahan-Nyong <rltgjqmduftlagl@gmail.com>
2022-06-20 00:01:33 +0900

    SOLVE :: 브루트 포스

--- algorithm/src/backjoon/day08/BOJ_2231.java
+++ algorithm/src/backjoon/day08/BOJ_2231.java
@@ -1,2 +1,21 @@
-package backjoon.day08;public class BOJ_2231 {
+package backjoon.day08;
+
+import java.util.Scanner;
+
+public class BOJ_2231 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int min = 0;
+
+        for (int i = 0; i < n; i ++){
+            int result = 0;
+            if (i != 0){
+                result = i%10;
+            }
+        }
+
+        System.out.println(min);
+    }
 }
--- algorithm/src/backjoon/day08/BOJ_2798.java
+++ algorithm/src/backjoon/day08/BOJ_2798.java
@@ -1,2 +1,35 @@
-package backjoon.day08;public class BOJ_2798 {
+package backjoon.day08;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BOJ_2798 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int sum = 0, max = 0;
+
+        for (int i = 0; i < n; i ++){
+            arr.add(sc.nextInt());
+        }
+
+        // 5 6 7 8 9
+        for (int i = 0; i < arr.size() - 1; i ++){
+            for (int j = i + 1; j < arr.size(); j ++){
+                for (int k = j + 1; k < arr.size(); k ++){
+                    if (arr.get(i) + arr.get(j) + arr.get(k) <= m){
+                        sum = arr.get(i) + arr.get(j) + arr.get(k);
+                        if (sum > max){
+                            max = sum;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
 }
