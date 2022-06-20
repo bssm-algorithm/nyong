d7a760b157c245726840112c3330dfa0814eeacf
Woowahan-Nyong <rltgjqmduftlagl@gmail.com>
2022-06-20 10:57:49 +0900

    SOLVE :: 그대로 출력하기 - 문자열

---
+++ algorithm/src/backjoon/day10/BOJ_11718.java
@@ -0,0 +1,16 @@
+package backjoon.day10;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s;
+
+        while((s = br.readLine()) != null){
+            System.out.println(s);
+        }
+    }
+}
