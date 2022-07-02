bf619a8e6cae952cbf1126a86c3f4afbf3ac894d
nyong <94087228+wonyongChoi05@users.noreply.github.com>
2022-07-02 23:49:37 +0900

    SOLVE :: 마이크로소프트 로고 - 구현

---
+++ algorithm/src/baekjoon/day22/BOJ_5338.java
@@ -0,0 +1,12 @@
+package baekjoon.day22;
+
+public class BOJ_5338 {
+    public static void main(String[] args) {
+
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
