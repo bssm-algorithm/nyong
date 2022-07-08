bbd20b13895f04129b8f22ae49174fa9f55e2443
nyong <94087228+wonyongChoi05@users.noreply.github.com>
2022-07-08 09:11:25 +0900

    SOLVE :: 숫자 카드 2 - 자료 구조, 정렬, 이진 탐색, 해시를 사용한 집합과 맵

---
+++ algorithm/src/baekjoon/day28/BOJ_10816.java
@@ -0,0 +1,71 @@
+package baekjoon.day28;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_10816 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < M; i++) {
+            int key = Integer.parseInt(st.nextToken());
+
+            sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+    private static int lowerBound(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+
+            int mid = (left + right) / 2;
+
+            if (key <= arr[mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static int upperBound(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+
+            int mid = (left + right) / 2;
+
+            if (key < arr[mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
