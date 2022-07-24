da6c1d7a46847398cea880d11443bbbe402b88bc
nyong <94087228+wonyongChoi05@users.noreply.github.com>
2022-07-24 18:08:34 +0900

    SOLVE :: 등록 - 구현

---
+++ algorithm/src/baekjoon/day34/BOJ_7287.java
@@ -0,0 +1,11 @@
+package baekjoon.day34;
+
+import java.io.IOException;
+
+public class BOJ_7287 {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(141);
+        System.out.print("rltgjqmduftlagl");
+    }
+}
